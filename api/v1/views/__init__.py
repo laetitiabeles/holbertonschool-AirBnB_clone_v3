bd2734ae2d6e2ebaad0f0cbccdd293b3b8ab3e71
laetitiabeles <laetitiabeles9@gmail.com>
2024-04-18 17:01:58 +0200

    fix docstring and imports

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -3,10 +3,9 @@
 '''
 Initialize the blueprint
 '''
-
 from flask import Blueprint
-from api.v1.views.index import *
 
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
+from api.v1.views.index import *
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 '''
-Docstring for the module
+Index for API routes
 '''
 
 
@@ -9,9 +9,9 @@ from flask import jsonify
 from api.v1.views import app_views
 
 
-@app_views.route('status', methods=['GET'])
+@app_views.route('status', methods=['GET'], strict_slashes=False)
 def status():
     '''
-    Return the status
+    Return the status of API
     '''
     return jsonify({"status": "OK"})
