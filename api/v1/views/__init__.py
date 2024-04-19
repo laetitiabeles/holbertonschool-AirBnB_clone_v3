56d9416104256ab3d5914b3bf3f085e89961e626
Khadaassi <aassikhadija@yahoo.fr>
2024-04-19 19:33:10 +0200

    Refactor API views initialization and imports in __init__.py

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,16 +1,11 @@
 #!/usr/bin/python3
-
-'''
-Initialize the blueprint
-'''
-
-# Importing Blueprint from Flask
+"""Define a Blueprint for API routes v1"""
 from flask import Blueprint
 
-# Creating a Blueprint named 'app_views' with URL prefix '/api/v1'
+
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
-# Importing views from different modules
+
 from api.v1.views.index import *
 from api.v1.views.states import *
 from api.v1.views.cities import *
